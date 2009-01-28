1542609530ae754970f5f31cac5dfb78ca4414bb
Christian Theune <ct@gocept.com>
2009-01-28 14:27:53 +0000

    release info

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ long_description = (
 
 setup(
     name='zope.lifecycleevent',
-    version='3.4.1dev',
+    version='3.4.1',
     url='http://pypi.python.org/pypi/zope.lifecycleevent',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
