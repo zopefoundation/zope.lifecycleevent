5a058103c347d11534c06656741123843b7815d6
Stephan Richter <stephan.richter@gmail.com>
2013-02-11 17:04:56 +0000

    Preparing release 4.0.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
 
 setup(
     name='zope.lifecycleevent',
-    version='4.0.0dev',
+    version='4.0.0',
     url='http://pypi.python.org/pypi/zope.lifecycleevent',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
