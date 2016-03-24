5f52c21a9b14b74eb2b7b929f5032c27b66ca372
Tres Seaver <tseaver@palladion.com>
2016-03-24 10:59:32 -0400

    Drop support for Python 2.6.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
 
 setup(
     name='zope.lifecycleevent',
-    version='4.1.1.dev0',
+    version='4.2.0.dev0',
     url='http://pypi.python.org/pypi/zope.lifecycleevent',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
@@ -37,7 +37,6 @@ setup(
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
